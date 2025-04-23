77f2442ee56fa34dabc76215bee16613d913eaba
Buzarewicz <marek.buzarewicz@gmail.com>
2025-04-23 10:54:47 +0200

    1.2 Corrected configuration.

--- INOapp/src/main/kotlin/com/mbuzarewicz/inoapp/FirebaseInitialization.kt
+++ INOapp/src/main/kotlin/com/mbuzarewicz/inoapp/FirebaseInitialization.kt
@@ -5,21 +5,26 @@ import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
+import java.io.ByteArrayInputStream
 import java.io.FileInputStream
 import java.io.InputStream
+import java.nio.charset.StandardCharsets
 
 
 @Service
-class FirebaseInitialization {
+class FirebaseInitialization(
+    @Value("\${firestore.credentials}")
+    private val firebaseCredentials: String,
+) {
 
     @Bean
     fun initialization(): Firestore {
 
-        val serviceAccount: InputStream =
-            FileInputStream("/Users/m.buzarewicz/IdeaProjects/INOapp-backend/INOapp/src/main/kotlin/com/mbuzarewicz/inoapp/serviceAccountKey.json")
-        val credentials = GoogleCredentials.fromStream(serviceAccount)
+        val credentialsStream = ByteArrayInputStream(firebaseCredentials.toByteArray(StandardCharsets.UTF_8))
+        val credentials = GoogleCredentials.fromStream(credentialsStream)
         val options = FirebaseOptions.Builder()
             .setCredentials(credentials)
             .build()
