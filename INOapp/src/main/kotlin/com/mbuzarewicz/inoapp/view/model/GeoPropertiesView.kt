99121aa5f341d0e228e9aebd65ac6d4eb6b941a5
m.buzarewicz <marek.buzarewicz@gmail.com>
2026-03-17 08:29:40 +0100

    2.27 Removed unused code.

--- INOapp/src/main/kotlin/com/mbuzarewicz/inoapp/view/model/GeoPropertiesView.kt
+++
@@ -1,9 +0,0 @@
-//package com.mbuzarewicz.inoapp.view.model
-// dodo
-//data class GeoPropertiesView(
-//    val id: String,
-//    val name: String,
-//    val type: String,
-//    val note: String,
-//    val accuracy: Double
-//)
